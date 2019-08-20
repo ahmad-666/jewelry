cef266a85d2b179d75235cbc2cb1428ce5d098f7
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-08-21 00:31:40 +0330

    start product.html

--- src/index/index.js
+++ src/index/index.js
@@ -2,7 +2,17 @@
 import '../../node_modules/@fortawesome/fontawesome-free/css/all.css' ;
 import '../init.scss' ;
 import './styles/landing.scss' ;
+import './styles/header.scss' ;
+import './styles/grid.scss' ;
+import './styles/slider.scss' ;
+import './styles/whyus.scss' ;
+import './styles/faq.scss' ;
 //load scripts
 import './scripts/landing' ;
+import './scripts/header' ;
+import './scripts/grid' ;
+import './scripts/slider' ;
+import './scripts/whyus' ;
+import './scripts/faq' ;
 //load videos 
 
