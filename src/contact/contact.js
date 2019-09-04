b20d04e29f1c71f55826890e3298b8333ff88c52
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-09-04 22:20:56 +0330

    contact admin

---
+++ src/contact/contact.js
@@ -0,0 +1,16 @@
+//load styles
+import '../../node_modules/@fortawesome/fontawesome-free/css/all.css' ;
+import '../init.scss' ;
+import '../index/styles/header.scss' ;
+import '../index/styles/nav.scss' ;
+import './styles/contact.scss' ;
+import '../index/styles/footer.scss' ;
+import '../index/styles/copyright.scss' ;
+//load scripts
+import '../index/scripts/header' ;
+import '../index/scripts/nav' ;
+import './scripts/contact' ;
+import '../index/scripts/footer' ;
+//load videos 
+
+
