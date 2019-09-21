e324d2a2d63d1ae6dde92d4bf51673150c4f80fa
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-09-21 15:53:49 -0700

    dash v2

---
+++ src/basket/basket.js
@@ -0,0 +1,17 @@
+//load styles
+import '../../node_modules/@fortawesome/fontawesome-free/css/all.css' ;
+import '../init.scss' ;
+import '../index/styles/header.scss' ;
+import '../index/styles/nav.scss' ;
+import '../orders/styles/main-menu.scss' ;
+import '../basket/styles/basket.scss' ;
+import '../index/styles/footer.scss' ;
+import '../index/styles/copyright.scss' ;
+//load scripts
+import '../index/scripts/header.js' ;
+import '../index/scripts/nav.js' ;
+import '../basket/scripts/basket.js' ;
+import '../orders/scripts/main-menu.js' ;
+import '../index/scripts/footer.js' ;
+//load videos 
+
